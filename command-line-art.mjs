374bb1be10604f0e5652c3beba8eaf0e5ceedb6e
Lorenzo Curreli <lorenzo.curreli99@gmail.com>
2022-10-07 09:14:01 +0200

    add command line art

---
+++ command-line-art.mjs
@@ -0,0 +1,10 @@
+import figlet from 'figlet';
+
+figlet('Welcome to my script', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
